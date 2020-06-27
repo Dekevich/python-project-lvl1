83122f6bb8cd9e43af7fe1c457bfce9847a964f9
Denis Dekevich <da.dekevich@gmail.com>
2020-06-28 00:05:06 +0300

    add welcome_user function

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,10 @@
+from brain_games.cli import welcome_user
+
+
 def main():
-    greeting = 'Welcome to the Brain Games!'
-    print(greeting)
+    print('Welcome to the Brain Games!')
+
+    welcome_user()
 
 
 if __name__ == '__main__':
