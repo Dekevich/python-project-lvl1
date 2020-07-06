7919d851db7e8eda9f6f6b120d7b7d8887b84efe
Denis Dekevich <da.dekevich@gmail.com>
2020-07-06 19:06:24 +0300

    add Brain Progression game

---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,54 @@
+import random
+
+from brain_games.game_engine import greet_user, run_game, show_results
+
+MISSING_ELEMENT = '..'
+
+
+def generate_progression(max_num=100, prog_length=10):
+    step = random.randint(1, 10)
+    first_element = random.randint(0, max_num)
+    last_element = first_element + step * prog_length
+    missing_element_index = random.randint(0, prog_length - 1)
+    progression = [
+        str(element) for element in
+        range(first_element, last_element + 1, step)
+    ]
+    progression[missing_element_index] = MISSING_ELEMENT
+
+    return ' '.join(progression)
+
+
+def find_missing_element(prog_str):
+    prog = [
+        int(element) if element.isdigit() else element
+        for element in prog_str.split(' ')
+    ]
+    missing_element_index = prog.index(MISSING_ELEMENT)
+    step = 1
+    for idx in range(len(prog) - 1):
+        if prog[idx] != MISSING_ELEMENT and prog[idx + 1] != MISSING_ELEMENT:
+            step = prog[idx + 1] - prog[idx]
+            break
+
+    if missing_element_index == 0:
+        return str(prog[1] - step)
+
+    return str(prog[missing_element_index - 1] + step)
+
+
+def brain_progression():
+    game_description = 'What number is missing in the progression?'
+    username = greet_user(game_description)
+
+    game_results = run_game(generate_progression, find_missing_element)
+
+    show_results(username, game_results)
+
+
+def main():
+    brain_progression()
+
+
+if __name__ == '__main__':
+    main()
