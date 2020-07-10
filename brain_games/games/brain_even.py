d87efa78746486c09777d116fe97a2622edde2b6
Denis Dekevich <da.dekevich@gmail.com>
2020-07-10 20:13:30 +0300

    fix typos in variable names

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -8,8 +8,8 @@ MAX_NUMBER = 100
 
 def get_question_and_correct_answer():
     question = random.randint(0, MAX_NUMBER)
-    corect_answer = 'yes' if is_even(question) else 'no'
-    return question, corect_answer
+    correct_answer = 'yes' if is_even(question) else 'no'
+    return question, correct_answer
 
 
 def get_player_answer():
