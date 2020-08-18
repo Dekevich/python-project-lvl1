dcdf53bee936f8c6346cea0dc9c1658cb217178b
Denis Dekevich <da.dekevich@gmail.com>
2020-08-18 11:15:52 +0300

    remove redundant function definition

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -10,9 +10,5 @@ def get_question_and_correct_answer():
     num1 = random.randint(0, MAX_NUMBER)
     num2 = random.randint(0, MAX_NUMBER)
     question = f'{num1} {num2}'
-    correct_answer = calculate_gcd(num1, num2)
+    correct_answer = math.gcd(num1, num2)
     return question, correct_answer
-
-
-def calculate_gcd(num1, num2):
-    return math.gcd(num1, num2)
